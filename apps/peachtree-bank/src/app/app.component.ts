30e41058db5e98c150e8fb7e46408c53438c5fa8
Octavian Okumu <octaviookumu@gmail.com>
2022-05-27 17:05:25 +0300

    Good riddance, story 4 done

--- apps/peachtree-bank/src/app/app.component.ts
+++ apps/peachtree-bank/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LayoutService } from '@backbase/ui-ang';
 
 @Component({
   selector: 'bootcamp-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'peachtree-bank';
+  constructor(
+    public readonly layoutService: LayoutService
+  ) {/**/}
 }
--- apps/peachtree-bank/src/app/app.module.ts
+++ apps/peachtree-bank/src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { IconModule, LayoutModule, LogoModule } from '@backbase/ui-ang';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    IconModule,
+    LayoutModule,
+    HttpClientModule,
+    LogoModule,
+    NgbDropdownModule],
   providers: [],
   bootstrap: [AppComponent],
 })
