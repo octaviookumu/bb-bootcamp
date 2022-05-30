8681ad69a55dc958f095ae7c1413ec8baa73be10
Octavian Okumu <octaviookumu@gmail.com>
2022-05-30 18:01:26 +0300

    Change title from links to Transactions

--- apps/peachtree-bank/src/app/account-transactions/account-transactions-bundle.module.ts
+++ apps/peachtree-bank/src/app/account-transactions/account-transactions-bundle.module.ts
@@ -1,15 +1,69 @@
 import { NgModule } from '@angular/core';
-import { AccountsTransactionsJourneyModule } from '@backbase/accounts-transactions-journey-ang';
+import {
+  AccountsDetailsComponent,
+  AccountsDetailsTabComponent,
+  AccountsListComponent,
+  AccountsManageComponent,
+  AccountsTransactionsJourneyComponent,
+  AccountsTransactionsJourneyModule,
+  TransactionDetailsComponent,
+  TransactionsListComponent,
+} from '@backbase/accounts-transactions-journey-ang';
 import { TemplateRegistry } from '@backbase/foundation-ang/core';
 import { PUBSUB, PubSubService } from '@backbase/foundation-ang/web-sdk';
 import {
   AccountsTransactionsJourneyConfiguration,
   AccountsTransactionsJourneyConfigurationToken,
 } from '@backbase/accounts-transactions-journey-ang';
-import { environment } from '../../environments/environment';
+import { Route } from '@angular/router';
+
+const customRoute: Route = {
+  path: '',
+  component: AccountsTransactionsJourneyComponent,
+  children: [
+    {
+      path: '',
+      redirectTo: 'list',
+      pathMatch: 'full',
+    },
+    {
+      path: 'list',
+      component: AccountsListComponent,
+      data: { title: 'My Accounts' },
+    },
+    {
+      path: 'manage',
+      component: AccountsManageComponent,
+    },
+    {
+      path: 'transactions',
+      component: AccountsDetailsTabComponent,
+      data: { title: 'Transactions' }, // change title from link to Transactions
+      children: [
+        { path: '', redirectTo: 'list', pathMatch: 'full' },
+        {
+          path: 'list',
+          component: TransactionsListComponent,
+          data: { title: 'Transactions' },
+          children: [
+            {
+              path: 'detail',
+              component: TransactionDetailsComponent,
+            },
+          ],
+        },
+        {
+          path: 'details',
+          component: AccountsDetailsComponent,
+          data: { title: 'Details' },
+        },
+      ],
+    },
+  ],
+};
 
 @NgModule({
-  imports: [AccountsTransactionsJourneyModule.forRoot()],
+  imports: [AccountsTransactionsJourneyModule.forRoot({ route: customRoute })],
   providers: [
     TemplateRegistry,
     // solves the pubsub error
@@ -21,7 +75,7 @@ import { environment } from '../../environments/environment';
     {
       provide: AccountsTransactionsJourneyConfigurationToken,
       useValue: <Partial<AccountsTransactionsJourneyConfiguration>>{
-        enableManageAccounts: environment.enableManageAccounts,
+        enableManageAccounts: true, // toggle manage accounts
       },
     },
   ],
