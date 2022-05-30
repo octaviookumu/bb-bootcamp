01d92bc1d2ae31561374aa21138c5ff2686731da
Octavian Okumu <octaviookumu@gmail.com>
2022-05-30 16:34:07 +0300

    Add data to transactions, mocks, configure, then sleep

--- apps/peachtree-bank/src/app/app.module.ts
+++ apps/peachtree-bank/src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 import { IconModule, LayoutModule, LogoModule } from '@backbase/ui-ang';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -19,8 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
     LayoutModule,
     HttpClientModule,
     LogoModule,
-    NgbDropdownModule],
-  providers: [],
+    NgbDropdownModule,
+  ],
+  providers: [...(environment.mockProviders || [])],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
